e26da766ce2888c9a7ea48c0d1f3f39221ab078e
audidas <audidas@naver.com>
2020-05-27 18:16:19 +0900

    Algorithm

---
+++ Algorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                // 입력
+                // 로직
+                // 렌더링
+            }
+
+        }
+    }
+}
