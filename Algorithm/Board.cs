d803f3673c9ec23855aae58cee229da113240e48
audidas <audidas@naver.com>
2020-05-28 17:33:38 +0900

    Tree

---
+++ Algorithm/Board.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    public class Board
+    {
+        public int[] _data = new int[25];
+        public List<int> _data2 = new List<int>();
+        public LinkedList<int> _data3 = new LinkedList<int>();
+        public void Initialize()
+        {
+            
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Algorithm
 {
@@ -6,10 +7,39 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            while(true){
+            Console.CursorVisible = false;
+            int lastTick = 0;
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+            while (true)
+            {
+                #region 프레임 관리 
+                int currentTick = System.Environment.TickCount;
+                int elapedTick = currentTick - lastTick;
+                // 만약에 경과한 시간이 1/30 초보다 작다면
+                if (elapedTick < WAIT_TICK)
+                {
+                    continue;
+                }
+                lastTick = currentTick;
+                #endregion
+                // FPS 프레임 (60프레임 OK 30프레임 이하로 NO)
+
                 // 입력
+
                 // 로직
-                // 렌더링
+
+
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        System.Console.ForegroundColor = ConsoleColor.Green;
+                        System.Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
             }
 
         }
