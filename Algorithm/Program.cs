3a45688e7477265379e5e7ffc3be4d5496f4b2d0
audidas <audidas@naver.com>
2020-05-29 18:04:13 +0900

    List Algorithm

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -7,17 +7,19 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
+            Board board = new Board();
+            board.Initialize();
+
             Console.CursorVisible = false;
-            int lastTick = 0;
             const int WAIT_TICK = 1000 / 30;
             const char CIRCLE = '\u25cf';
+            int lastTick = 0;
             while (true)
             {
                 #region 프레임 관리 
                 int currentTick = System.Environment.TickCount;
-                int elapedTick = currentTick - lastTick;
                 // 만약에 경과한 시간이 1/30 초보다 작다면
-                if (elapedTick < WAIT_TICK)
+                if (currentTick - lastTick < WAIT_TICK)
                 {
                     continue;
                 }
@@ -28,8 +30,6 @@ namespace Algorithm
                 // 입력
 
                 // 로직
-
-
                 Console.SetCursorPosition(0, 0);
                 for (int i = 0; i < 25; i++)
                 {
